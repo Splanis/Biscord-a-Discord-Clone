7bce049fe0e8af1e8a093d75bd58b0b2d0b42cdc
splanis <splanis5@gmail.com>
2020-07-06 17:53:24 +0300

    added previous user to posts, fixed date

--- client/src/pages/server/Posts.js
+++ client/src/pages/server/Posts.js
@@ -3,20 +3,42 @@ import React from "react";
 import Post from "./Post";
 
 const Posts = ({ posts, auth_token }) => {
+    const divRef = React.useRef(null);
+
+    React.useEffect(() => {
+        divRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [posts]);
+
     return (
         <div
             style={{
                 flex: 300,
                 display: "flex",
                 flexDirection: "column",
-                justifyContent: "flex-end",
                 overflowY: "auto",
             }}
         >
             {posts ? (
-                posts.map((post) => (
-                    <Post key={post._id} post={post} auth_token={auth_token} />
-                ))
+                <>
+                    {posts.map((post, index) =>
+                        index > 0 ? (
+                            <Post
+                                key={post._id}
+                                post={post}
+                                prevOwner={posts[index - 1].owner}
+                                auth_token={auth_token}
+                            />
+                        ) : (
+                            <Post
+                                key={post._id}
+                                post={post}
+                                prevOwner={null}
+                                auth_token={auth_token}
+                            />
+                        )
+                    )}
+                    <div ref={divRef} />
+                </>
             ) : (
                 <p>The are no answers in this Channel</p>
             )}
