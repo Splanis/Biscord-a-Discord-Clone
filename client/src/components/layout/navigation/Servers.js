e73293abe93055bcbad07c0db881aa0de6f2a4aa
splanis <splanis5@gmail.com>
2020-07-20 19:34:54 +0300

    removed unnecessary imports

--- client/src/components/layout/navigation/Servers.js
+++ client/src/components/layout/navigation/Servers.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import {
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Button,
-    IconButton,
-} from "@material-ui/core";
+import { ListItem } from "@material-ui/core";
 
 const Servers = () => {
     const servers = useSelector((state) =>
